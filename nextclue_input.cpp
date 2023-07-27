322fe0ae48ae88fc3261199df72de74bf37ab4bc
刘佳鹏 <jaguarliu@tencent.com>
2023-07-27 20:31:55 +0800

    add code4file

--- nextclue_input.cpp
+++ nextclue_input.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
 using namespace std;
-
+//Level 9
 int main()
 {
-
-    cout << "This file was useless, so I made it better..." << endl;
-    while(1);
+    cout << "hope you resolved all your conflicts!!" << endl;
     return 0;
 }
 
